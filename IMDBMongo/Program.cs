a8a7baa868475671ead7dd38da8d7b42785ca6ed
move <move@zealand.dk>
2022-03-29 10:23:55 +0200

    Add project files.

---
+++ IMDBMongo/Program.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace IMDBMongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IMDB Mongo tests!");
+
+            var client = new MongoClient();
+
+            var databases = client.ListDatabases().ToList();
+
+            //foreach (var database in databases)
+            //{
+            //    Console.WriteLine(database);
+            //}
+
+            var IMDB = client.GetDatabase("IMDB");
+
+            var collections = IMDB.ListCollections().ToList();
+
+            //foreach (var collection in collections)
+            //{
+            //    Console.WriteLine(collection);
+            //}
+
+            var titlesbasic = IMDB.GetCollection<BsonDocument>("TitlesBasic");
+
+            var titleList = titlesbasic.Find("{}").Limit(10).ToList();
+
+            var joinedList = titlesbasic
+                .Aggregate()
+                .Limit(10)
+                .Lookup("principals", "tconst", "tconst", "principals")
+                .ToList();
+            
+            foreach (var title in joinedList)
+            {
+                Console.WriteLine(title);
+            }
+        }
+    }
+}
